d42aa009a5942fde098e68b55ad68e2ab97fe62e
Prieena <priyalama709@gmail.com>
2024-09-30 18:18:35 +0530

    spring boot microservices

---
+++ spring boot microservice/second-micro-service-client/src/test/java/com/example/second_micro_service_client/SecondMicroServiceClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.second_micro_service_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondMicroServiceClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
